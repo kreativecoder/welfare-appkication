cdfd22a38ccf09cc091878699c8ff9306368612d
jerrycaffe <jerrycaffe@gmail.com>
2021-02-26 00:49:31 +0100

    update to config file

--- config/config.js
+++ config/config.js
@@ -18,9 +18,7 @@ dotenv.config();
       use_env_variable: 'DATABASE_URL',
       url: process.env.DATABASE_URL,
       dialect: 'postgres',
-      dialectOptions: {
-        ssl: true
-      },
+      ssl: { rejectUnauthorized: false }
      
     }
   }
