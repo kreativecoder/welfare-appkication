3d03f188595f26e650c47b7f3a8b03addb6c66fc
jerrycaffe <jerrycaffe@gmail.com>
2021-04-03 08:43:18 +0100

    worked on database configuration

--- config/config.js
+++ config/config.js
@@ -1,37 +1,34 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-  
-  module.exports =  {development: {
+
+module.exports = {
+  development: {
     // url: process.env.DATABASE_URL,
-    
-      username: "xkjvkmjouvynvq",
-      password: "cc52018c321647370ab23b6688f2c887ef0990eeca89b1c0aa0f3cc6d5c51dfa",
-      database: "d1o5s813gt1b2p",
-      host: "ec2-18-204-74-74.compute-1.amazonaws.com",
-      port: 5432,
-      dialect: "postgres",
-      operatorsAliases: false
+
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    // username: 'xkjvkmjouvynvq',
+    // password: "cc52018c321647370ab23b6688f2c887ef0990eeca89b1c0aa0f3cc6d5c51dfa",
+    // database: "d1o5s813gt1b2p",
+    // host: "ec2-18-204-74-74.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres",
+    operatorsAliases: false,
+  },
+  test: {
+    storage: "./database.sqlite",
+    dialect: "sqlite",
+    logging: false,
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-    test: {
-      storage: './database.sqlite',
-      dialect: 'sqlite',
-      logging: false
-    },
-    production: {
-      use_env_variable: 'DATABASE_URL',
-      // url: 'postgres://xkjvkmjouvynvq:cc52018c321647370ab23b6688f2c887ef0990eeca89b1c0aa0f3cc6d5c51dfa@ec2-18-204-74-74.compute-1.amazonaws.com:5432/d1o5s813gt1b2p?sslmode=require'
-      
-      // username: "xkjvkmjouvynvq",
-      // password: "cc52018c321647370ab23b6688f2c887ef0990eeca89b1c0aa0f3cc6d5c51dfa",
-      // database: "d1o5s813gt1b2p",
-      // host: "ec2-18-204-74-74.compute-1.amazonaws.com",
-      // port: 5432,
-      // dialect: "postgres",
-      // operatorsAliases: false,
-      // url: process.env.DATABASE_URL,
-      // dialect: 'postgres'
-      
-      
-    }
-  }
-  
+  },
+};
--- config/dbconnection.js
+++ config/dbconnection.js
@@ -5,37 +5,32 @@ import { config } from "dotenv";
 config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  // "d1o5s813gt1b2p",
+  // "xkjvkmjouvynvq",
+  // "cc52018c321647370ab23b6688f2c887ef0990eeca89b1c0aa0f3cc6d5c51dfa",
   {
-    host: process.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-    // username: "xkjvkmjouvynvq",
-    // password: "cc52018c321647370ab23b6688f2c887ef0990eeca89b1c0aa0f3cc6d5c51dfa",
-    // database: "d1o5s813gt1b2p",
-    // port: 5432,
+    host: process.env.DB_HOST,
     // host: "ec2-18-204-74-74.compute-1.amazonaws.com",
-    // dialectOptions: {
-    //   ssl: {
-    //     require: false,
-    //     rejectUnauthorized: false // <<<<<< YOU NEED THIS
-    //   }
-    // },
-    // port: 5432,
-    // dialect: "postgres",
-    // ssl: {
-    //   rejectUnauthorized: true
-    // },
- 
-   
-   
+
+    dialect: "postgres",
+    dialectOptions: process.env.NODE_ENV === "production" && {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
+    logging: false,
   }
 );
 
 async function connect() {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log("Database Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
